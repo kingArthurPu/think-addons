b31bc6d2a38f3fbd36115eebf6ef8c493f7fb38d
hongming pu <puhongming1253@gmail.com>
2019-07-29 23:23:03 +0800

    更换插件核心代码

---
+++ src/addons/Route.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace think\addons;
+
+use think\facade\Hook;
+
+/**
+ * 插件执行默认控制器
+ * Class AddonsController
+ * @package think\addons
+ */
+class Route extends Controller
+{
+    # 资源类型
+    protected $mimeType = [
+        'xml' => 'application/xml,text/xml,application/x-xml',
+        'json' => 'application/json,text/x-json,application/jsonrequest,text/json',
+        'js' => 'text/javascript,application/javascript,application/x-javascript',
+        'css' => 'text/css',
+        'rss' => 'application/rss+xml',
+        'yaml' => 'application/x-yaml,text/yaml',
+        'atom' => 'application/atom+xml',
+        'pdf' => 'application/pdf',
+        'text' => 'text/plain',
+        'png' => 'image/png',
+        'jpg' => 'image/jpg,image/jpeg,image/pjpeg',
+        'gif' => 'image/gif',
+        'csv' => 'text/csv',
+        'html' => 'text/html,application/xhtml+xml,*/*',
+    ];
+
+    /**
+     * 插件执行
+     */
+    public function execute()
+    {
+        if (!empty($this->addon) && !empty($this->controller) && !empty($this->action)) {
+            // 获取类的命名空间
+            $class = get_addon_class($this->addon, 'controller', $this->controller, $this->module);
+            if (class_exists($class)) {
+                $model = new $class();
+                if ($model === false) {
+                    abort(500, lang('addon init fail'));
+                }
+                // 调用操作
+                if (!method_exists($model, $this->action)) {
+                    abort(500, lang('Controller Class Method Not Exists'));
+                }
+                // 监听addons_init
+                Hook::listen('addons_init', $this);
+                return call_user_func_array([$model, $this->action], [$this->request]);
+            } else {
+                abort(500, lang('Controller Class Not Exists'));
+            }
+        }
+        abort(500, lang('addon cannot name or action'));
+    }
+
+    /**
+     * 样式读取
+     */
+    public function assets()
+    {
+        $assets_path = dirname(__DIR__, 5);
+        $path = $this->request->pathinfo();
+        $len = strlen('assets/' . $this->module);
+        $ext = $this->request->ext();
+        if ($ext) {
+            $type = 'text/html';
+            $content = file_get_contents($assets_path . '/addons/' . $this->module . '/assets' . substr($path, $len));
+            if (array_key_exists($ext, $this->mimeType)) {
+                $type = $this->mimeType[$ext];
+            }
+            return response($content, 200, ['Content-Length' => strlen($content)])->contentType($type);
+        }
+    }
+
+
+}
--- src/helper.php
+++ src/helper.php
@@ -1,8 +1,6 @@
 <?php
 
-
 use think\facade\App;
-use think\facade\Env;
 use think\facade\Hook;
 use think\facade\Config;
 use think\Loader;
@@ -13,51 +11,10 @@ use think\facade\Route;
 $appPath = App::getAppPath();
 $addons_path = dirname($appPath) . DIRECTORY_SEPARATOR . 'addons' . DIRECTORY_SEPARATOR;
 Env::set('addons_path', $addons_path);
+// 定义路由
+Route::any('addons/execute/:addon-[:module]-:control-:action', "\\think\\addons\\Route@execute");
+Route::any('assets_[:module]', "\\think\\addons\\Route@assets");
 
-// 插件访问路由配置
-Route::group('addons', function () {
-    if (!isset($_SERVER['REQUEST_URI'])) {
-        return 'error addons';
-    }
-    // 请求位置
-    $path = ltrim(parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH), '/');
-    $ext = pathinfo($path, PATHINFO_EXTENSION);
-    if ($ext) {
-        $path = substr($path, 0, strlen($path) - (strlen($ext) + 1));
-    }
-    $pathinfo = explode('/', $path);
-    // 路由地址
-    if ($pathinfo[0] == 'addons' && isset($pathinfo[2])) {
-        // 获取路由地址
-        $route = explode('.', $pathinfo[1]);
-        $module = array_shift($route);
-        $className = ucfirst(array_pop($route));
-        array_push($route, $className);
-        $controller = join('\\', $route);
-        $type = array_shift($route);
-        // 生成view_path
-        $view_path = Env::get('addons_path') . $module . DIRECTORY_SEPARATOR . 'view' . DIRECTORY_SEPARATOR;
-        Config::set('template.view_path', $view_path);
-        // 中间件
-        $middleware = [];
-        $config = Config::get('addons.middleware');
-        if (is_array($config) && isset($config[$type])) {
-            $middleware = (array)$config[$type];
-        }
-        // 请求转入
-        Route::rule(':rule', "\\addons\\{$module}\\controller\\{$controller}@{$pathinfo[2]}")
-            ->middleware($middleware);
-    }
-})->middleware(function ($request, \Closure $next) {
-    // 路由地址
-    $pathinfo = explode('/', $request->path());
-    $rules = explode('.', $pathinfo[1]);
-    $request->setModule(array_shift($rules));
-    $request->setController(join('/', $rules));
-    $request->setAction($pathinfo[2]);
-
-    return $next($request);
-});
 
 // 如果插件目录不存在则创建
 if (!is_dir($addons_path)) {
@@ -71,6 +28,7 @@ Loader::addNamespace('addons', $addons_path);
 Hook::add('app_init', function () {
     // 获取开关
     $autoload = (bool)Config::get('addons.autoload', false);
+
     // 非正是返回
     if (!$autoload) {
         return;
@@ -80,23 +38,18 @@ Hook::add('app_init', function () {
     if (empty($config)) {
         // 读取插件目录及钩子列表
         $base = get_class_methods("\\think\\Addons");
-
-
         // 读取插件目录中的php文件
         foreach (glob(Env::get('addons_path') . '*/*.php') as $addons_file) {
             // 格式化路径信息
             $info = pathinfo($addons_file);
-
             // 获取插件目录名
             $name = pathinfo($info['dirname'], PATHINFO_FILENAME);
             // 找到插件入口文件
-            if (strtolower($info['filename']) == 'widget') {
+            if (strtolower($info['filename']) == strtolower($name)) {
                 // 读取出所有公共方法
                 $methods = (array)get_class_methods("\\addons\\" . $name . "\\" . $info['filename']);
                 // 跟插件基类方法做比对，得到差异结果
                 $hooks = array_diff($methods, $base);
-
-
                 // 循环将钩子方法写入配置中
                 foreach ($hooks as $hook) {
                     if (!isset($config['hooks'][$hook])) {
@@ -114,16 +67,16 @@ Hook::add('app_init', function () {
         }
         cache('addons', $config);
     }
-    Config::set("addons.hooks",$config['hooks']);
-
+    config('addons.hooks', $config["hooks"]);
 });
 
 // 闭包初始化行为
 Hook::add('action_begin', function () {
     // 获取系统配置
     $data = App::isDebug() ? [] : Cache::get('hooks', []);
-   $config = config('addons.');
+    $config = config('addons.');
     $addons = isset($config['hooks']) ? $config['hooks'] : [];
+
     if (empty($data)) {
         // 初始化钩子
         foreach ($addons as $key => $values) {
@@ -132,7 +85,7 @@ Hook::add('action_begin', function () {
             } else {
                 $values = (array)$values;
             }
-            $addons[$key] = array_filter(array_map('get_addons_class', $values));
+            $addons[$key] = array_filter(array_map('get_addon_class', $values));
             Hook::add($key, $addons[$key]);
         }
         cache('hooks', $addons);
@@ -149,11 +102,7 @@ Hook::add('action_begin', function () {
  */
 function hook($hook, $params = [])
 {
-    $result = Hook::listen($hook, $params);
-    if (is_array($result)) {
-        $result = join(PHP_EOL, $result);
-    }
-    return $result;
+    Hook::listen($hook, $params);
 }
 
 /**
@@ -163,8 +112,8 @@ function hook($hook, $params = [])
  * @param string $class 当前类名
  * @return string
  */
-if (!function_exists('get_addons_class')) {
-    function get_addons_class($name, $type = 'hook', $class = null)
+if (!function_exists("get_addon_class")) {
+    function get_addon_class($name, $type = 'hook', $class = null, $module = '')
     {
         $name = Loader::parseName($name);
         // 处理多级控制器情况
@@ -179,15 +128,15 @@ if (!function_exists('get_addons_class')) {
         }
         switch ($type) {
             case 'controller':
-                $namespace = "\\addons\\" . $name . "\\controller\\" . $class;
+                $namespace = "\\addons\\" . $name . ($module ? '\\' . $module : '') . "\\controller\\" . $class;
                 break;
             default:
-                $namespace = "\\addons\\" . $name . "\\Widget";
+                $namespace = "\\addons\\" . $name . ($module ? '\\' . $module : '') . "\\" . $class;
         }
-
         return class_exists($namespace) ? $namespace : '';
     }
 }
+
 
 /**
  * 获取插件类的配置文件数组
@@ -246,7 +195,7 @@ if (!function_exists('get_addons_config')) {
 if (!function_exists('get_addons_info')) {
     function get_addons_info($name)
     {
-        $class = "\\addons\\{$name}\\Widget";
+        $class = "\\addons\\{$name}\\{$name}";
         if (!class_exists($class)) {
             return [];
         }
@@ -256,28 +205,106 @@ if (!function_exists('get_addons_info')) {
 }
 
 /**
+ * 插件显示内容里生成访问插件的url
+ * @param $url
+ * @param array $param
+ * @param bool|string $suffix 生成的URL后缀
+ * @param bool|string $domain 域名
+ * @return bool|string
+ */
+if (!function_exists("addon_url")) {
+    function addons_url($url, $param = [], $suffix = true, $domain = false)
+    {
+        $url = parse_url(strtolower($url));
+        $case = config('url_convert');
+        $addons = $case ? Loader::parseName($url['scheme']) : $url['scheme'];
+        if (isset($url['user']) && !empty($url['user'])) {
+            $user = $case ? Loader::parseName($url['user']) : $url['user'];
+        }
+        $controller = $case ? Loader::parseName($url['host']) : $url['host'];
+        $action = trim($case ? strtolower($url['path']) : $url['path'], '/');
+        /* 解析URL带的参数 */
+        if (isset($url['query'])) {
+            parse_str($url['query'], $query);
+            $param = array_merge($query, $param);
+        }
+
+        // 生成插件链接新规则
+        $actions = "{$addons}" . (isset($user) ? '-' . $user : '') . "-{$controller}-{$action}";
+
+        return url("addons/execute/{$actions}", $param, $suffix, $domain);
+    }
+}
+
+/**
+ * 静态资源解析路径
+ * @param string $url
+ */
+if (!function_exists("assets_url")) {
+    function assets_url($url)
+    {
+        return "/assets_" . $url;
+    }
+}
+
+
+/**
  * 生成插件配置文件
  * @param array $config 配置信息
  * @param string $name 配置文件名
  * @return array
  */
-if(!function_exists('create_config')){
-    function create_config($config,$name="config.php"){
-        $config_file=Env::get("addons_path").$config['name'].DIRECTORY_SEPARATOR.$name;
+if (!function_exists('create_config')) {
+    function create_config($config, $name = "config.php")
+    {
+        $config_file = Env::get("addons_path") . $config['name'] . DIRECTORY_SEPARATOR . $name;
 
-        if(is_file($config_file) && file_exists($config_file)){
+        if (is_file($config_file) && file_exists($config_file)) {
             return false;
         }
-        $config=var_export($config, true);
-        $content =<<<EOT
-    <?php
+        $config = var_export($config, true);
+        $content = <<<EOT
+<?php
     return {$config};
 EOT;
-        $result=file_put_contents($config_file,$content);
-        if($result===false){
+        $result = file_put_contents($config_file, $content);
+        if ($result === false) {
             return false;
         }
         return true;
+    }
+}
+
+/**
+ * 获取插件类的类名
+ * @param $name 插件名
+ * @param string $type 返回命名空间类型
+ * @param string $class 当前类名
+ * @return string
+ */
+if (!function_exists('get_addons_class')) {
+    function get_addons_class($name, $type = 'hook', $class = null)
+    {
+        $name = Loader::parseName($name);
+        // 处理多级控制器情况
+        if (!is_null($class) && strpos($class, '.')) {
+            $class = explode('.', $class);
+            foreach ($class as $key => $cls) {
+                $class[$key] = Loader::parseName($cls, 1);
+            }
+            $class = implode('\\', $class);
+        } else {
+            $class = Loader::parseName(is_null($class) ? $name : $class, 1);
+        }
+        switch ($type) {
+            case 'controller':
+                $namespace = "\\addons\\" . $name . "\\controller\\" . $class;
+                break;
+            default:
+                $namespace = "\\addons\\" . $name . "\\{$name}";
+        }
+
+        return class_exists($namespace) ? $namespace : '';
     }
 }
 
@@ -299,7 +326,7 @@ if (!function_exists("get_addons_list")) {
             }
             // 定义插件有效性的规则：
             // 1 插件名称是否首字母大写
-            $file = $addonsPath . $item . DIRECTORY_SEPARATOR . ucfirst("widget") . '.php';
+            $file = $addonsPath . $item . DIRECTORY_SEPARATOR . ucfirst($item) . '.php';
             if (!is_file($file)) {
                 continue;
             }
@@ -315,7 +342,7 @@ if (!function_exists("get_addons_list")) {
             }
             $info = $class->info;
             // 判断是否生成了config.php文件
-            if(is_file($addonsPath . $item . DIRECTORY_SEPARATOR.'config.php')){
+            if (is_file($addonsPath . $item . DIRECTORY_SEPARATOR . 'config.php')) {
                 $info['status'] = 1;
             }
             $addonsList[] = $info;
@@ -325,38 +352,4 @@ if (!function_exists("get_addons_list")) {
 }
 
 
-/**
- * 插件显示内容里生成访问插件的url
- * @param $url
- * @param array $param
- * @param bool|string $suffix 生成的URL后缀
- * @param bool|string $domain 域名
- * @return bool|string
- */
-if (!function_exists('addons_url')) {
-    function addons_url($url = '', $param = [], $suffix = true, $domain = false)
-    {
-        if (empty($url)) {
-            // 生成 url 模板变量
-            $addons = request()->module();
-            $controller = request()->controller();
-            $controller = str_replace('/', '.', $controller);
-            $action = request()->action();
-        } else {
-            $url = Loader::parseName($url, 1);
-            $url = parse_url($url);
-            $case = config('url_convert');
-            $addons = $case ? Loader::parseName($url['scheme']) : $url['scheme'];
-            $controller = $case ? Loader::parseName($url['host']) : $url['host'];
-            $action = trim($case ? strtolower($url['path']) : $url['path'], '/');
 
-            /* 解析URL带的参数 */
-            if (isset($url['query'])) {
-                parse_str($url['query'], $query);
-                $param = array_merge($query, $param);
-            }
-        }
-
-        return url("addons/{$addons}.{$controller}/{$action}", $param, $suffix, $domain);
-    }
-}
