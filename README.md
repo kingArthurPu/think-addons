154b139cdf7d152b00b7c61da7800b6ec4b01df5
kingarthur.pu <kingarthur.pu@qq.com>
2019-07-31 18:17:21 +0800

    获取插件名称 模块 控制器 方法

--- README.md
+++ README.md
@@ -1,39 +1,170 @@
-# kingarthur-addons
+# kingarthur/think-addons
 
 #### 介绍
-{**以下是码云平台说明，您可以替换此简介**
-码云是 OSCHINA 推出的基于 Git 的代码托管平台（同时支持 SVN）。专为开发者提供稳定、高效、安全的云端软件开发协作平台
-无论是个人、团队、或是企业，都能够用码云实现代码托管、项目管理、协作开发。企业项目请看 [https://gitee.com/enterprises](https://gitee.com/enterprises)}
+基于thinkPHP5.1的插件扩展
 
-#### 软件架构
-软件架构说明
 
 
 #### 安装教程
 
-1. xxxx
-2. xxxx
-3. xxxx
-
-#### 使用说明
-
-1. xxxx
-2. xxxx
-3. xxxx
-
-#### 参与贡献
-
-1. Fork 本仓库
-2. 新建 Feat_xxx 分支
-3. 提交代码
-4. 新建 Pull Request
+composer require kingarthur/think-addons
 
 
-#### 码云特技
+安装以后运行框架，将自动在框架根目录下生成addons目录;
 
-1. 使用 Readme\_XXX.md 来支持不同的语言，例如 Readme\_en.md, Readme\_zh.md
-2. 码云官方博客 [blog.gitee.com](https://blog.gitee.com)
-3. 你可以 [https://gitee.com/explore](https://gitee.com/explore) 这个地址来了解码云上的优秀开源项目
-4. [GVP](https://gitee.com/gvp) 全称是码云最有价值开源项目，是码云综合评定出的优秀开源项目
-5. 码云官方提供的使用手册 [https://gitee.com/help](https://gitee.com/help)
-6. 码云封面人物是一档用来展示码云会员风采的栏目 [https://gitee.com/gitee-stars/](https://gitee.com/gitee-stars/)
+
+#### 配置
+
+在config目录下新增配置文件`addons.php`:
+```php
+    <?php
+
+        return [
+            // 是否自动读取取插件钩子配置信息（默认是关闭）
+            'autoload' => false,
+            // 当关闭自动获取配置时需要手动配置hooks信息
+            'hooks' => [
+                // 可以定义多个钩子
+                'testhook'=>'test' // 键为钩子名称，用于在业务中自定义钩子处理，值为实现该钩子的插件，
+                // 多个插件可以用数组也可以用逗号分割
+            ]
+        ];
+```
+
+#### 创建插件
+
+> 创建的插件可以在view视图中使用，也可以在php业务逻辑中使用
+
+在addons目录中创建test目录，目录名称test即为插件名称;
+
+在test中创建`Widget.php`,代码如下:
+
+```php
+<?php
+
+    namespace addons\test;	// 注意命名空间规范
+    use think\Addons;
+    use think\App;
+    use think\facade\Env;
+
+    /**
+    * 插件测试
+    * @author byron sampson
+    */
+    class Widget extends Addons	// 需继承think\addons\Addons类
+    {
+        // 该插件的基础信息
+        public $info = [
+            'name' => 'test',	// 插件标识
+            'title' => '插件测试',	// 插件名称
+            'description' => 'thinkph5插件测试',	// 插件简介
+            'status' => 0,	// 状态
+            'author' => 'zhangsan',
+            'version' => '0.1'
+        ];
+
+        /**
+        * 插件安装方法
+        * @return bool
+        */
+        public function install()
+        {
+            return true;
+        }
+
+        /**
+        * 插件卸载方法
+        * @return bool
+        */
+        public function uninstall()
+        {
+            return true;
+        }
+
+        /**
+        * 实现的testhook钩子方法
+        * @return mixed
+        */
+        public function testhook($param)
+        {
+            // 调用钩子时候的参数信息
+            print_r($param);
+            // 当前插件的配置信息，配置信息存在当前目录的config.php文件中，见下方
+            print_r($this->getConfig());
+            $this->assign("name","kingarthur-addons");
+            echo  $this->fetch("info");
+        }
+    }
+```
+
+#### 创建钩子模板文件
+
+在`test`目录下新建`info.html`视图文件:
+```html
+    <body>
+        <h1>hello tpl---</h1>
+
+        如果插件中需要有链接或提交数据的业务，可以在插件中创建controller业务文件，
+        要访问插件中的controller时使用addons_url生成url链接。
+        如下：
+        <a href="{:addons_url('test://Action/link',['g'=>'xxxxx'])}">link test</a>
+        格式为：
+        test为插件名，Action为controller中的类名，link为controller中的方法
+        <hr>
+        {$name}
+    </body>
+```
+
+#### 创建插件的controller文件
+
+> 在test目录中创建controller目录，在controller目录中创建Index.php文件 controller类的用法与tp5.1中的controller一致
+
+```php
+<?php
+namespace addons\test\controller;
+
+use think\addons\Controller;
+
+class Action extends Controller
+{
+    public function link()
+    {
+        return $this->fetch();
+    }
+}
+```
+
+
+
+#### 使用钩子
+
+> 创建好插件后就可以在正常业务中使用该插件中的钩子了 使用钩子的时候第二个参数可以省略
+
+- 模板中使用钩子
+
+```html
+<div>{:hook('testhook', ['id'=>1])}</div>
+```
+
+- php业务中使用
+
+```php
+hook('testhook', ['id'=>1])
+```
+
+
+另外，可以在`test`目录中创建`model`以及`validate`，使用方式同模块中使用一致
+
+
+
+#### 特别感谢
+
+zzstudio/think-addons
+
+[gitee地址](https://github.com/zz-studio/think-addons)
+
+
+
+OkCoder/think-addons
+
+[gitee地址](https://gitee.com/okcoder/think-addons)
--- src/Addons.php
+++ src/Addons.php
@@ -1,14 +1,10 @@
 <?php
 
 namespace think;
-
 use think\facade\Env;
-use think\addons\Controller;
-
 /**
  * 插件基类
  * Class Addns
- * @author Byron Sampson <xiaobo.sun@qq.com>
  * @package think\addons
  */
 abstract class Addons extends Controller
@@ -22,7 +18,7 @@ abstract class Addons extends Controller
      *  'title'         => '测试插件',
      *  'description'   => '用于thinkphp5的插件扩展演示',
      *  'status'        => 1,
-     *  'author'        => 'byron sampson',
+     *  'author'        => 'kingarthur',
      *  'version'       => '0.1'
      * ]
      */
@@ -42,6 +38,12 @@ abstract class Addons extends Controller
 //        }
     }
 
+    //必须实现安装
+    abstract public function install();
+
+    //必须卸载插件方法
+    abstract public function uninstall();
+
     /**
      * 获取插件信息
      * @return array
@@ -58,16 +60,14 @@ abstract class Addons extends Controller
         return $this->info;
     }
 
-    /**
-     * 获取插件的配置数组
-     * @param string $name 可选模块名
-     * @return array|mixed|null
-     */
-    final public function getConfig($parse = false)
+    // 获取扩展项目的配置信息
+    public function getConfig()
     {
-        $name = $this->getName();
-        return get_addons_config($name, $parse);
+        $file = env('addons_path') . $this->getAddonName() . '/config.php';
+        if (!file_exists($file)) return false;
+        return require_once $file;
     }
+
 
     /**
      * 获取当前模块名
@@ -77,6 +77,64 @@ abstract class Addons extends Controller
     {
         $data = array_reverse(explode('\\', get_class($this)));
         return $data[1];
+    }
+
+    // 获取扩展配置基本信息
+    public function getManifest()
+    {
+        $file = env('addons_path') . $this->getAddonName() . '/manifest.php';
+        if (!file_exists($file)) return false;
+        return require_once $file;
+    }
+
+    // 获取继承者的类名称  不含Addon的小写字母名称
+    protected function getAddonName()
+    {
+        $arr = explode('\\', get_class($this));
+        return $arr[1];
+    }
+
+    // 创建安装成功标识文件
+    protected function createInstallFile()
+    {
+        $file = env('addons_path') . $this->getAddonName() . '/install.lock';
+        if (file_exists($file)) return true;
+        return file_put_contents($file, 'dir', FILE_USE_INCLUDE_PATH);
+    }
+
+    protected function deleteInstallFile()
+    {
+        $file = env('addons_path') . $this->getAddonName() . '/install.lock';
+        if (!file_exists($file)) return true;
+        return unlink($file);
+    }
+
+    /**
+     * 重写模板渲染
+     * @param string $template
+     * @param array $vars
+     * @param array $config
+     * @return mixed|string
+     * @throws \Exception
+     */
+    protected function fetch($template = '', $vars = [], $config = [])
+    {
+        // 拼接插件视图文件的默认目录
+        return $this->view->fetch($this->addons_path.$template.'.'.config('template.view_suffix'), $vars, $config);
+    }
+
+
+    /**
+     * 创建扩展
+     * @param string $addonName
+     * @param int $type 0单模块 1多模块
+     * @param array $config 配置
+     */
+    public function createAddon($addonName = '', $type = 0, $config = [])
+    {
+        $addonName = strtolower($addonName);
+        $filePath = env('addons_path') . $addonName;
+        file_put_contents($filePath . '/config.php');
     }
 
     /**
@@ -94,36 +152,5 @@ abstract class Addons extends Controller
         return true;
     }
 
-    /**
-     * 重写模板渲染
-     * @param string $template
-     * @param array $vars
-     * @param array $config
-     * @return mixed|string
-     * @throws \Exception
-     */
-    protected function fetch($template = '', $vars = [], $config = [])
-    {
-//        if(!\is_file($template)){
-//            $template = '/'.$template;
-//        }
-        echo $this->addons_path;
-        return $this->view->fetch($this->addons_path.$template.'.'.config('template.view_suffix'), $vars, $config);
-    }
-
-    /**
-     * 获取当前错误信息
-     * @return mixed
-     */
-    public function getError()
-    {
-        return $this->error;
-    }
-
-    //必须实现安装
-    abstract public function install();
-
-    //必须卸载插件方法
-    abstract public function uninstall();
 }
     
